f4c326afec29966b2cfc3952bfbee22f42068643
MatiasMaron <101577603+MatiasMaron@users.noreply.github.com>
2022-08-30 10:41:53 -0300

    Tp 7 Consignas 1, 2 y 3 hechas

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] GameObject UIElements;
+    [SerializeField] TextMeshProUGUI dialogueTxt;
+    [SerializeField] string[] NPCDialogue;
+    [SerializeField] NPCDialogue NPCDialogueScript;
+    int dialogueIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        NPCDialogueScript = other.gameObject.GetComponent<NPCDialogue>();
+        if (NPCDialogueScript)
+        {
+            UIElements.SetActive(true);
+            NPCDialogue = NPCDialogueScript.data.dialogueLines;
+            ShowNextDialogueLine();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            UIElements.SetActive(false);
+            dialogueIndex = 0;
+        }
+    }
+
+    public void ShowNextDialogueLine()
+    {
+        if (dialogueIndex < NPCDialogue.Length)
+        {
+            dialogueTxt.text = NPCDialogue[dialogueIndex];
+            dialogueIndex++;
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Crear Player Data So",fileName ="PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    public int coins;
+}
